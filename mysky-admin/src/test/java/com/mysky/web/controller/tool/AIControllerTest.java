c89e48d88383818fb589afa43a6a46e1e269e7da
ll <1397136470@qq.com>
2025-01-13 18:40:19 +0800

    feat:添加测试用例demo

---
+++ mysky-admin/src/test/java/com/mysky/web/controller/tool/AIControllerTest.java
@@ -0,0 +1,22 @@
+package com.mysky.web.controller.tool;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AIControllerTest {
+
+    @Test
+    public void testOnePlusOneEqualsTwo() {
+        assertEquals(2, 1 + 1);
+        assertEquals(3, 1 + 2);
+
+        // This test will fail
+        assertEquals(4, 1 + 3);
+
+        System.out.println("testOnePlusOneEqualsTwo");
+        System.out.println("testOnePlusOneEqualsTwo");
+
+
+    }
+} 
